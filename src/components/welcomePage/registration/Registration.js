f5a5295944e6274a44d01bdbe3a9acace622cc18
Adis2020 <adiskd22gm@gmail.com>
2023-10-15 17:28:27 +0600

    add: added form

--- src/components/welcomePage/registration/Registration.js
+++ src/components/welcomePage/registration/Registration.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import s from './Registration.module.css';
 
 const Registration = () => {
     return (
-        <div>
-            
+        <div className={s.registration}>
+            <div className={s.mainText}>Регистрация</div>
+            <div>Имя: <input type="text"/></div>
+            <div>Фамилия: <input type="text"/></div>
+            <div>Пароль: <input type="text"/></div>
+            <button className={s.submit}>Зарегистрироваться</button>
         </div>
     );
 };
