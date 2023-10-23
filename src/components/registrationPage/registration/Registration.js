aa0ea94b28d617aa6f3f78b2cd5c4061086489bb
Adis2020 <adiskd22gm@gmail.com>
2023-10-23 17:18:57 +0600

    added: function for get data about user

--- src/components/registrationPage/registration/Registration.js
+++ src/components/registrationPage/registration/Registration.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import s from './Registration.module.css';
+import {NavLink} from "react-router-dom";
 
 const Registration = () => {
+    const getInfo = (e) => {
+        e.preventDefault();
+        const data = {
+            name: e.target.name.value,
+            secondName: e.target.secondName.value,
+            password: e.target.password.value
+        }
+        console.log(data)
+    }
+
     return (
-        <div className={s.registration}>
+        <form className={s.registration} onSubmit={getInfo}>
             <div className={s.mainText}>Регистрация</div>
-            <div>Имя: <input type="text"/></div>
-            <div>Фамилия: <input type="text"/></div>
-            <div>Пароль: <input type="text"/></div>
-            <button className={s.submit}>Зарегистрироваться</button>
-        </div>
+            <div>Имя: <input type="text" id="name"/></div>
+            <div>Фамилия: <input type="text" id="secondName"/></div>
+            <div>Пароль: <input type="text" id="password"/></div>
+            <NavLink to={"/main"}>
+                <button type="submit" className={s.submit}>Зарегистрироваться</button>
+            </NavLink>
+        </form>
     );
 };
 
